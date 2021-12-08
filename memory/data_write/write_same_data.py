e0e79334c926d019fffa101e92015b04428328c1
Javier Parada <javierparada73@outlook.com>
2021-12-08 11:48:02 +0100

    add write file script

--- memory/data_write/write_same_data.py
+++ memory/data_write/write_same_data.py
@@ -1,11 +1,15 @@
+import time
 
 if __name__ == '__main__':
     num = 0
     text_to_write = ''
-    for i in range(10000):
+    for i in range(200000):
         text_to_write = f"{text_to_write}potatoe\n"
     while True:
         num = num + 1
         f = open(f"the-file{num}.txt", "w+")
         f.write(text_to_write)
         f.close()
+        if num % 10 == 0:
+            print('10 files done.')
+        time.sleep(0.1)
